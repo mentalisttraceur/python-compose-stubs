9aafbb1db6dbd305433175b1ee464ba520ffcdb7
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-08-17 18:47:18 +0000

    Support 1-255 arguments to `compose`

---
+++ generate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+arguments = 'f1: Callable[P, R1], /'
+overloads = [f'def compose({arguments}) -> Callable[P, R1]']
+
+for number in range(2, 256):
+    arguments = f'f{number}: Callable[[R{number-1}], R{number}], {arguments}'
+    overloads.append(f'def compose({arguments}) -> Callable[P, R{number}]')
+
+print('from typing import Callable, TypeVar, ParamSpec, overload')
+print("P = ParamSpec('P')")
+
+for number in range(1, 256):
+    print(f"R{number} = TypeVar('R{number}')")
+
+for signature in reversed(overloads):
+    print('@overload')
+    print(f'{signature}:')
+    print('    ...')
