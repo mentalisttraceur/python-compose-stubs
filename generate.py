13c594efdc9a3bbd95e2a905a084a104f7e8abc6
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-08-17 20:57:03 +0000

    Fix alphabetical import sort

--- generate.py
+++ generate.py
@@ -7,7 +7,7 @@ for number in range(2, 256):
     arguments = f'f{number}: Callable[[R{number-1}], R{number}], {arguments}'
     overloads.append(f'def compose({arguments}) -> Callable[P, R{number}]')
 
-print('from typing import Callable, TypeVar, ParamSpec, overload')
+print('from typing import Callable, ParamSpec, TypeVar, overload')
 print("P = ParamSpec('P')")
 
 for number in range(1, 256):
