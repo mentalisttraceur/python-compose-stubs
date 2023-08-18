69303e13b6ed96177aead95789716fcf5b87f4c5
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2023-08-18 01:54:24 +0000

    Reduce arity supported from 256 to 16
    This drastically optimizes the realistic and
    common cases - seemingly instant checks instead
    of perceptible hangs (for many seconds in error
    cases when doing non-daemonized/non-incremental
    checking). As much as I hate it, this is the
    better tradeoff.

--- generate.py
+++ generate.py
@@ -21,7 +21,7 @@ def _overloads(name, callable_, return_):
     argument = callable_('P', 'R1')
     arguments = f'f1: {argument}, /'
     yield _overload(name, arguments, return_('P', 'R1'))
-    for number in range(2, 256):
+    for number in range(2, 16):
         argument = callable_(f'[R{number-1}]', f'R{number}')
         arguments = f'f{number}: {argument}, {arguments}'
         yield _overload(name, arguments, return_('P', f'R{number}'))
@@ -31,7 +31,7 @@ print('from typing import'
       + ' Awaitable, Callable, ParamSpec, TypeVar, Union, overload')
 print("P = ParamSpec('P')")
 
-for number in range(1, 256):
+for number in range(1, 16):
     print(f"R{number} = TypeVar('R{number}')")
 
 for x in _overloads('compose', _sync_callable, _sync_callable):
